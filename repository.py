763c1f1796d94d1870477a958bd47c5eba16496c
fabgal-p1 <>
2024-05-02 19:06:25 +0200

    - Add get_data in repository

--- repository.py
+++ repository.py
@@ -1,6 +1,7 @@
 import os
 
 from helpers_config import get_serialized_data
+from helpers_financialmodelingprep import get_financial_data
 
 
 def get_config():
@@ -9,11 +10,19 @@ def get_config():
     return get_serialized_data(config_path)
 
 
-def get_data():
-    pass
+def get_data(tickers):
+    return get_financial_data(tickers)
 
 
 if __name__ == "__main__":
     config = get_config()
     print(config)
     print("End config test")
+
+    tickers = ["AAPL", "MSFT", "NVDA"]
+
+    incom_stat, balance_sheet, cash_flow = get_data(tickers)
+    print(incom_stat.shape)
+    print(balance_sheet.shape)
+    print(cash_flow.shape)
+
